64e8342cb006bb1a7cb175763c55b437d1af084c
doviboy <109406957+doviboy@users.noreply.github.com>
2022-07-16 15:18:14 +0530

    Update QA-SQ.js
    new

--- QA-SQ.js
+++ QA-SQ.js
@@ -1,5 +1,5 @@
 module.exports = {
-    QUEEN_AMDI_OWNER: '94757405652', // Black Amda - Queen Amdi Owner
+    QUEEN_AMDI_OWNER: '94769965447', // dovi boy - Queen Amdi Owner
     QUEEN_AMDI_TEAM: '94719077818,94757672873,94774976567,94766426385,94711870791', // Team - Queen Amdi TEAM
-    SEW_QUEEN_OWNER: '94785435462' // Ravindu Manoj - Sew Queen Owner --> For The Friendship 
+    SEW_QUEEN_OWNER: '94769965447' // Ravindu Manoj - Sew Queen Owner --> For The Friendship 
 };
